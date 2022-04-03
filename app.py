1d94e909fa86955800224f3e4cf42fadad9ff9b5
Vignesh S <95733152+Vsekar05@users.noreply.github.com>
2022-04-03 15:17:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ import streamlit.components.v1 as components
 from babel.numbers import format_currency
 import gunicorn
 from sklearn.model_selection import train_test_split
-import unzip
-unzip /content/model.zip
+
 
 def main():
   st.set_page_config(page_title="House-Price-Predictor",page_icon="house.jpg",layout="centered",initial_sidebar_state="auto",menu_items=None)
